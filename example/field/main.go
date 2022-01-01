89309a4830a676d5a1df396ebeeddb63973af1fc
Christian Weichel <chris@gitpod.io>
2022-01-01 16:00:12 +0000

    Correctly incorporate bleed when tracing the field

--- example/field/main.go
+++ example/field/main.go
@@ -21,12 +21,12 @@ func main() {
 		rand.Seed(4589)
 		for i := 0; i < 500; i++ {
 			s := plot.XY{X: rand.Intn(p.Size.X), Y: rand.Intn(p.Size.Y)}
-			d = append(d, traceLine(f, s, 10, 100)...)
+			d = append(d, traceLine(p, f, s, 10, 100)...)
 		}
 		rand.Seed(189)
 		for i := 0; i < 500; i++ {
 			s := plot.XY{X: rand.Intn(p.Size.X), Y: rand.Intn(p.Size.Y)}
-			d = append(d, traceLine(f, s, 10, 100)...)
+			d = append(d, traceLine(p, f, s, 10, 100)...)
 		}
 
 		d = append(d, plot.AsDebug(p.Frame()...)...)
@@ -36,7 +36,7 @@ func main() {
 	})
 }
 
-func traceLine(field *field.VectorField, start plot.XY, len, steps int) (r []plot.Drawable) {
+func traceLine(p plot.Canvas, field *field.VectorField, start plot.XY, len, steps int) (r []plot.Drawable) {
 	if steps <= 0 {
 		return
 	}
@@ -48,9 +48,9 @@ func traceLine(field *field.VectorField, start plot.XY, len, steps int) (r []plo
 	angle := nearest.Angle
 	end := start.Add(int(float64(len)*math.Cos(angle)), int(float64(len)*math.Sin(angle)))
 	r = append(r, plot.Line{
-		Start: start,
-		End:   end,
+		Start: start.AddXY(p.Bleed),
+		End:   end.AddXY(p.Bleed),
 	})
-	r = append(r, traceLine(field, end, len, steps-1)...)
+	r = append(r, traceLine(p, field, end, len, steps-1)...)
 	return
 }
