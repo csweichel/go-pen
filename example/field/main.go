0093290033eecad282a31a705aad30e0d8fe9b12
Christian Weichel <chris@gitpod.io>
2022-01-01 15:24:25 +0000

    Vector fields are now sampled and stored more efficiently

--- example/field/main.go
+++ example/field/main.go
@@ -10,27 +10,14 @@ func main() {
 		Size:  plot.A4.Mult(4),
 		Bleed: plot.XY{X: 15, Y: 40},
 	}, func(p plot.Canvas, args map[string]string) (d plot.Drawing, err error) {
-		const (
-			cx = 20
-			cy = 40
-		)
+		counts := plot.XY{X: 40, Y: 80}
+		// spacing := plot.XY{X: p.Inner().X / (counts.X - 1), Y: p.Inner().Y / (counts.Y - 1)}
 
-		// var vs []field.PointVector
-		// for x := 0; x < cx; x++ {
-		// 	for y := 0; y < cy; y++ {
-		// 		vs = append(vs, field.PointVector{
-		// 			P:      p.Zero().Add((p.Inner().X/cx)*x, (p.Inner().Y/cy)*y),
-		// 			Length: 10,
-		// 			Angle:  math.Pi / cy * float64(y),
-		// 		})
-		// 	}
-		// }
-
-		// f := field.NewVectorField(vs)
-		// d = append(d, f.Draw()...)
-
-		f := field.NewPerlinNoiseField(p, plot.XY{X: cx, Y: cy}, 2, 2, 3, 1000)
-		d = append(d, f.Draw()...)
+		// d = append(d, field.NewVectorField(counts, spacing, func(p plot.XY) field.Vector {
+		// 	return field.Vector{Length: 10, Angle: 0.5}
+		// }).Draw(p.Bleed)...)
+		f := field.NewPerlinNoiseField(p, counts, 2, 2, 3, 1000)
+		d = append(d, f.Draw(p.Bleed)...)
 
 		d = append(d, plot.AsDebug(p.Frame()...)...)
 		d = append(d, plot.AsDebug(p.FrameBleed()...)...)
--- pkg/field/field.go
+++ pkg/field/field.go
@@ -9,56 +9,70 @@ import (
 )
 
 // NewVectorField produces a new vector field from vectors
-func NewVectorField(points []PointVector) *VectorField {
-	return &VectorField{Points: points}
+func NewVectorField(counts, spacing plot.XY, sampler func(p plot.XY) Vector) *VectorField {
+	res := make([][]Vector, counts.X)
+	for x := 0; x < counts.X; x++ {
+		res[x] = make([]Vector, counts.Y)
+		for y := 0; y < counts.Y; y++ {
+			res[x][y] = sampler(plot.XY{X: x * spacing.X, Y: y * spacing.Y})
+		}
+	}
+	return &VectorField{
+		Points:  res,
+		Spacing: spacing,
+	}
 }
 
 // VectorField represents a vector field
 type VectorField struct {
-	Points []PointVector
+	Points  [][]Vector
+	Spacing plot.XY
 }
 
+// func (f *VectorField) Nearest(p plot.XY) Vector {
+
+// }
+
 // Draw draws the field as a set of lines
-func (field *VectorField) Draw() []plot.Drawable {
-	res := make([]plot.Drawable, len(field.Points))
-	for i, p := range field.Points {
-		res[i] = plot.Line{
-			Start: p.P,
-			End:   p.P.AddF(float64(p.Length)*math.Sin(p.Angle), float64(p.Length)*math.Cos(p.Angle)),
+func (field *VectorField) Draw(offset plot.XY) []plot.Drawable {
+	res := make([]plot.Drawable, 0, len(field.Points))
+	for x, xs := range field.Points {
+		for y, p := range xs {
+			s := plot.XY{X: x*field.Spacing.X + offset.X, Y: y*field.Spacing.Y + offset.Y}
+			res = append(res, plot.Line{
+				Start: s,
+				End:   s.AddF(float64(p.Length)*math.Sin(p.Angle), float64(p.Length)*math.Cos(p.Angle)),
+			})
 		}
 	}
 	return res
 }
 
-// PointVector represents a vector with a starting point, angle and length
-type PointVector struct {
-	P      plot.XY
+// Vector represents a vector with a starting point, angle and length
+type Vector struct {
 	Angle  float64
 	Length int
 }
 
 // NewPerlinNoiseField produces a new vector field spread across the canvas, where the angles are determined
-// using Perlin noise. The delta determines the number of points in the vector field along X and Y.
+// using Perlin noise. The counts determines the number of points in the vector field along X and Y.
 // Alpha, beta, n and seed are perlin noise generator parameters.
-func NewPerlinNoiseField(p plot.Canvas, delta plot.XY, alpha, beta float64, n int32, seed int64) *VectorField {
-	gen := perlin.NewPerlin(alpha, beta, n, seed)
-	vec := make([]PointVector, 0, delta.X*delta.Y)
+func NewPerlinNoiseField(p plot.Canvas, counts plot.XY, alpha, beta float64, n int32, seed int64) *VectorField {
 	var (
-		inner = p.Inner()
-		dx    = float64(inner.X) / float64(delta.X)
-		dy    = float64(inner.Y) / float64(delta.Y)
+		inner   = p.Inner()
+		dx      = float64(inner.X) / float64(counts.X-1)
+		dy      = float64(inner.Y) / float64(counts.Y-1)
+		spacing = plot.XY{X: int(dx), Y: int(dy)}
 	)
-	for x := 0; x < delta.X; x++ {
-		for y := 0; y < delta.Y; y++ {
-			pos := plot.XY{X: int(dx*float64(x)) + p.Bleed.X, Y: int(dy*float64(y)) + p.Bleed.Y}
-			ns := gen.Noise2D(float64(pos.X)/float64(inner.X), float64(pos.Y)/float64(inner.Y))
-			angle := 2 * math.Pi * ns
-			vec = append(vec, PointVector{
-				P:      pos,
-				Angle:  angle,
-				Length: 10,
-			})
+
+	gen := perlin.NewPerlin(alpha, beta, n, seed)
+	return NewVectorField(counts, spacing, func(pos plot.XY) Vector {
+		ns := gen.Noise2D(float64(pos.X)/float64(inner.X), float64(pos.Y)/float64(inner.Y))
+		angle := 2 * math.Pi * ns
+
+		return Vector{
+			Angle:  angle,
+			Length: 35,
 		}
-	}
-	return NewVectorField(vec)
+	})
 }
